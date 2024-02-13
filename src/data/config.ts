62bb1365284f288c9febaa8822b03971830499c0
moreshk <moreshk@gmail.com>
2024-02-13 12:48:25 +1100

    env variable for candymachine address

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,1 +1,2 @@
-export const config = { mintAddress: 'HrDFFFacT6xVUUEBWDjvGvR6z1eDuKd2egtQsA8p3yd2' }
+// export const config = { mintAddress: 'HrDFFFacT6xVUUEBWDjvGvR6z1eDuKd2egtQsA8p3yd2' }
+export const config = { mintAddress: process.env.NEXT_PUBLIC_MINT_ADDRESS || 'default_address' }
