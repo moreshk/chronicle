0881bbfd962e823209e5bc03c2665d3fefce7f58
Pranav <pranavkp.me@outlook.com>
2024-02-15 14:58:15 +0530

    fix the seconds

--- src/app/nft/[nftAddress]/chat/showCreditTimer.tsx
+++ src/app/nft/[nftAddress]/chat/showCreditTimer.tsx
@@ -28,15 +28,13 @@ const CountdownTimer: React.FC = () => {
     }
   }, [seconds]);
 
+  const formattedTime = formatTime(seconds);
+
   return (
     <>
       {seconds > 0 ? (
         <div className="border-4 bg-red-900/10 rounded-2xl border-red-900/50 text-red-700/80 px-6 py-2 mt-1">
-          <p>
-            Not enough credits. Credits will reset in{" "}
-            {Math.floor(seconds / 60) ? `${seconds / 60} minutes` : ""}{" "}
-            {(seconds % 60).toFixed(0)} seconds
-          </p>
+          <p>Not enough credits. Credits will reset in {formattedTime}</p>
         </div>
       ) : null}
     </>
@@ -44,3 +42,29 @@ const CountdownTimer: React.FC = () => {
 };
 
 export default CountdownTimer;
+
+function formatTime(seconds: number): string {
+  const days = Math.floor(seconds / (24 * 3600));
+  const hours = Math.floor((seconds % (24 * 3600)) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  const formatUnit = (value: number, unit: string): string => {
+    if (value === 1) {
+      return `${value} ${unit}`;
+    } else if (value > 1) {
+      return `${value} ${unit}s`;
+    }
+    return "";
+  };
+
+  const formattedTime = `${formatUnit(days, "day")} ${formatUnit(
+    hours,
+    "hour"
+  )} ${formatUnit(minutes, "minute")} ${formatUnit(
+    remainingSeconds,
+    "second"
+  )}`.trim();
+
+  return formattedTime;
+}
